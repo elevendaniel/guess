0398090d34143bb340b83a2ad3096fe5beba3b29
danielliu <danielliu@DanieldeMacBook-Pro.local>
2019-02-14 12:29:51 +0800

    v3

--- guess2.py
+++ guess2.py
@@ -5,17 +5,25 @@
 #r = random.randint(1, 100) #randint = random +integer（整数）
 #设计一个猜数字的程式
 import random
-r = random. randint(1, 100)
+start = input('请决定随机数字范围开始值：')
+end = input('请决定随机数字范围结束值：')
+start = int (start)
+end = int (end)
+r = random. randint(start, end)
+count = 0 
 while True:
+	count += 1 # count = count +1 是一样的意思 简写的方式
 	m = input('请猜数字：')
 	m = int(m)
 	if m == r :
 		print('恭喜你猜对了！')
+		print('这是你猜的第', count ,'次')
 		break
 	elif m > r :
 		print('你猜的大了')
 	elif m < r :
 		print('你猜的小了')
+	print('这是你猜的第', count ,'次')
 
 
 
